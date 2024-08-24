8209716e306674aa79fb0f175f81608d05902a33
heligp <heligonzalespe@gmail.com>
2024-08-24 14:30:08 -0500

    add init setups
    listas las configuraciones de un nuevo proyecto el cual incluye un droplet y su respectiva configuracion de networking

--- main.tf
+++ main.tf
@@ -15,3 +15,67 @@ variable "do_token" {}
 provider "digitalocean" {
   token = var.do_token
 }
+
+# Create a new Web Droplet in the nyc1 region
+resource "digitalocean_droplet" "web" {
+  image    = "ubuntu-20-04-x64"
+  name     = "basictfm"
+  region   = "nyc3"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = ["3d:0b:15:15:61:9d:35:81:b1:10:89:ca:b2:ea:97:57"]
+}
+
+
+resource "digitalocean_firewall" "web" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_project" "playground" {
+  name        = "playground"
+  description = "A project to represent development resources."
+  purpose     = "Web Application"
+  environment = "Development"
+  resources   = [digitalocean_droplet.web.urn]
+}
